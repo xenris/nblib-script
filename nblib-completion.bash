08d0d54f69381aeeeb467eb5bf303ffcb0aa4e61
Henry Shepperd <hshepperd@gmail.com>
2020-07-15 00:42:15 +1000

    Initial commit

---
+++ nblib-completion.bash
@@ -0,0 +1,5 @@
+#/usr/bin/env bash
+
+complete -W "version create build clean upload memory create serial update help --help" ./nblib
+# complete -A directory "./nblib create"
+# complete -A directory "./nblib serial"
